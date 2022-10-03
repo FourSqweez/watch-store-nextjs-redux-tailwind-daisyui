a9334d93add557f7a4a5a539eeb10790e89e845e
FourSqweez <four7518@gmail.com>
2022-10-03 22:01:15 +0700

    add : wrapped provider to app to alow app to manage data from global state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import store from '../app/store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
+		<Provider store={store}>
 			<Navbar />
 			<Component {...pageProps} />
-		</>
+		</Provider>
 	)
 }
 
