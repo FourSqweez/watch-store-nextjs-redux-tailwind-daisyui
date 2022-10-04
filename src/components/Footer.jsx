53542f0924a51769252ff806948489813f96dd75
FourSqweez <four7518@gmail.com>
2022-10-04 12:13:49 +0700

    add : created and add footer to _app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className=" flex justify-center items-center w-full h-[54px] bg-[#1A586A]">
+			<p className="text-[#F5F5F5] font-[400] text-[12px] leading-[14px] tracking-wide">
+				Copyright © 2022 ChomCHOB. All Rights Reserved.
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import store from '../app/store'
 import { Provider } from 'react-redux'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<Navbar />
 			<Component {...pageProps} />
+			<Footer />
 		</Provider>
 	)
 }
