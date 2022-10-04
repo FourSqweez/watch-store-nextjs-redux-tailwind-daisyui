3d95f2d4aa38d65955314b89704fc034367aefa1
FourSqweez <four7518@gmail.com>
2022-10-04 15:16:37 +0700

    fixed some bug styled

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -37,8 +37,8 @@ const Cart = () => {
 	}
 
 	return (
-		<div className="flex w-full h-screen justify-center">
-			<div className="max-w-[1050px] mb-10 mt-20 mx-2">
+		<div className="flex w-full h-screen md:h-full justify-center">
+			<div className="max-w-[1050px] mb-10  mt-20 mx-2">
 				{cart.length === 0 ? (
 					<div className="flex h-full flex-col items-center justify-center gap-4">
 						<h2 className="flex text-center justify-center">
@@ -63,7 +63,7 @@ const Cart = () => {
 									cart.map((item) => (
 										<div key={item.id}>
 											<div className="flex justify-between h-full items-start border-b-2 py-2 first:border-t-2">
-												<div className="flex  h-full items-center ml-2">
+												<div className="flex h-full items-center ml-2">
 													<div className="mr-2 h-20 w-20 min-w-[4rem]  relative overflow-hidden rounded-md">
 														<Image
 															layout="fill"
